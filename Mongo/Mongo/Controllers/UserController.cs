fa740604ab360af93682bb69b47610809583f047
Dmytro Didusenko <didusenko.dmytro@gmail.com>
2023-05-06 21:49:35 +0300

    Updated User controller

--- Mongo/Mongo/Controllers/UserController.cs
+++ Mongo/Mongo/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Mongo.DataManager;
+using Mongo.Models;
 
 namespace Mongo.Controllers
 {
@@ -9,22 +10,64 @@ namespace Mongo.Controllers
     {
         private readonly MongoDataManager _dataManager;
 
-        public UserController(MongoDataManager dataManager)
-        {
+        public UserController(MongoDataManager dataManager) => 
             _dataManager = dataManager;
-        }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<List<User>> Get() =>
+        await _dataManager.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<User>> Get(string id)
         {
-            try
+            var user = await _dataManager.GetAsync(id);
+
+            if (user is null)
             {
-                return Ok(await _dataManager.GetAsync());
+                return NotFound();
             }
-            catch (Exception ex)
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(User newUser)
+        {
+            await _dataManager.CreateAsync(newUser);
+
+            return CreatedAtAction(nameof(Get), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, User updatedUser)
+        {
+            var user = await _dataManager.GetAsync(id);
+
+            if (user is null)
             {
-                return BadRequest(ex.Message);
+                return NotFound();
             }
+
+            updatedUser.Id = user.Id;
+
+            await _dataManager.UpdateAsync(id, updatedUser);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _dataManager.GetAsync(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            await _dataManager.RemoveAsync(id);
+
+            return NoContent();
         }
     }
 }
